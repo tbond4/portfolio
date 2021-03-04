4ee3c69d3ce15ec54303e78efebd62462f0a2d41
Tristan Bond <tristanebond@gmail.com>
2021-03-04 15:41:05 -0800

    initial file set up and basic components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header(){
+return(
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">Tristan Bond</h1>
+            <p className="lead">Welcome to my Portfolio</p>
+        </div>
+    </div>
+);
+}
+
+export default Header;
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,14 @@
+function ProjectCard({title, image, desc, link}){
+    return(
+        <div className="card mb-3">
+        <img src={image} className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{desc}</p>
+          <a href={link} className="btn btn-light">Visit</a>
+        </div>
+      </div>
+    );
+}
+
+export default ProjectCard;
